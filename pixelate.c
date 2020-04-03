098141bd180da0428a4f9a243ea68648384cd4e9
5265644D61736F6E <r2acayan@gmail.com>
2020-04-03 10:58:38 -0400

    added logic to find distance between colors

--- pixelate.c
+++ pixelate.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 /*
   pixelate bitmap image buf (in place)
 
@@ -9,7 +12,7 @@
   pallete_len	- length of the color list
 */
 
-void pixelate(char* buf,int channels,int c_width,int c_height,int t_width,int t_height,char* palette,int palette_len) {
+void pixelate(unsigned char* buf,int channels,int c_width,int c_height,int t_width,int t_height,unsigned char* palette,int palette_len) {
   int* dists;
 
   int y;
@@ -20,8 +23,8 @@ void pixelate(char* buf,int channels,int c_width,int c_height,int t_width,int t_
 
   dists = malloc(c_width * c_height * palette_len * sizeof(int));
 
-  for (y = 0;y < c_height;y++)
-    for (x = 0;x < c_width;x++)
+  for (y = 0;y < c_height;y++) {
+    for (x = 0;x < c_width;x++) {
       for (i = 0;i < palette_len;i++) {
 	// initialize value
 	dists[y * c_width * palette_len
@@ -31,13 +34,55 @@ void pixelate(char* buf,int channels,int c_width,int c_height,int t_width,int t_
 	for (j = 0;j < channels;j++) {
 	  // add square of difference
 
-	  a = palette[i * channels + j] - buf[y * c_width * channels + x * channels];
-
-	  a *= a;
-
+	  a = (int) buf[y * c_width * channels + x * channels + j] - (int) palette[i * channels + j];
 	  dists[y * c_width * palette_len
 	      + x * palette_len
-	      + i] += a;
+	      + i] += a * a;
 	}
+
+	printf("%d,",dists[y * c_width * palette_len
+			 + x * palette_len
+		         + i]);
       }
+
+      printf("\t");
+    }
+
+    printf("\n");
+  }
+
+  free(dists); // clean up
+}
+
+int test() {
+  char img[48] = {
+    0x00,0x00,0x00,
+    0x10,0x10,0x10,
+    0x20,0x20,0x20,
+    0x30,0x30,0x30,
+    0x40,0x40,0x40,
+    0x50,0x50,0x50,
+    0x60,0x60,0x60,
+    0x70,0x70,0x70,
+    0x80,0x80,0x80,
+    0x90,0x90,0x90,
+    0xA0,0xA0,0xA0,
+    0xB0,0xB0,0xB0,
+    0xC0,0x00,0x00,
+    0xD0,0x00,0x00,
+    0xE0,0x00,0x00,
+    0xF0,0x00,0x00
+  };
+
+  char palette[9] = {
+    0x00,0x00,0x00,
+    0xFF,0x00,0x00,
+    0xFF,0xFF,0xFF
+  };
+
+  pixelate(img,3,4,4,4,4,palette,3);
+}
+
+int main() {
+  test();
 }
