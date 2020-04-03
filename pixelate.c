9cf2cbee148d968cf04e2caaa98fe2b83dfc3473
5265644D61736F6E <r2acayan@gmail.com>
2020-04-02 22:26:33 -0400

    separated declarations and assignments

--- pixelate.c
+++ pixelate.c
@@ -10,20 +10,28 @@
 */
 
 void pixelate(char* buf,int channels,int c_width,int c_height,int t_width,int t_height,char* palette,int palette_len) {
-  int* dists = malloc(c_width * c_height * palette_len * sizeof(int));
+  int* dists;
 
-  for (int y = 0;y < c_height;y++)
-    for (int x = 0;x < c_width;x++)
-      for (int i = 0;i < palette_len;i++) {
+  int y;
+  int x;
+  int i;
+  int j;
+  int a;
+
+  dists = malloc(c_width * c_height * palette_len * sizeof(int));
+
+  for (y = 0;y < c_height;y++)
+    for (x = 0;x < c_width;x++)
+      for (i = 0;i < palette_len;i++) {
 	// initialize value
 	dists[y * c_width * palette_len
 	    + x * palette_len
 	    + i] = 0;
 
-	for (int j = 0;j < channels;j++) {
+	for (j = 0;j < channels;j++) {
 	  // add square of difference
 
-	  int a = palette[i * channels + j] - buf[y * c_width * channels + x * channels];
+	  a = palette[i * channels + j] - buf[y * c_width * channels + x * channels];
 
 	  a *= a;
 
