279efb6207b6e3cd482ed6b499c95ba179ece26c
5265644D61736F6E <r2acayan@gmail.com>
2020-04-02 22:22:01 -0400

    added logic to find distances between colors

--- pixelate.c
+++ pixelate.c
@@ -10,5 +10,26 @@
 */
 
 void pixelate(char* buf,int channels,int c_width,int c_height,int t_width,int t_height,char* palette,int palette_len) {
-  
+  int* dists = malloc(c_width * c_height * palette_len * sizeof(int));
+
+  for (int y = 0;y < c_height;y++)
+    for (int x = 0;x < c_width;x++)
+      for (int i = 0;i < palette_len;i++) {
+	// initialize value
+	dists[y * c_width * palette_len
+	    + x * palette_len
+	    + i] = 0;
+
+	for (int j = 0;j < channels;j++) {
+	  // add square of difference
+
+	  int a = palette[i * channels + j] - buf[y * c_width * channels + x * channels];
+
+	  a *= a;
+
+	  dists[y * c_width * palette_len
+	      + x * palette_len
+	      + i] += a;
+	}
+      }
 }
