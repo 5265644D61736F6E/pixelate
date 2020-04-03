f68d1d07460f8fc9fb30e7cfff77e4ab2b5dd802
5265644D61736F6E <r2acayan@gmail.com>
2020-04-02 21:29:19 -0400

    stub function with minimal documentation

---
+++ pixelate.c
@@ -0,0 +1,14 @@
+/*
+  pixelate bitmap image buf (in place)
+
+  c_width	- current width of bitmap image
+  c_height	- current height of bitmap image
+  t_width	- target level-of-detail width of output image
+  t_height	- target level-of-detail height of output image
+  palette	- list of colors to snap to
+  pallete_len	- length of the color list
+*/
+
+void pixelate(char* buf,int channels,int c_width,int c_height,int t_width,int t_height,char* palette,int palette_len) {
+  
+}
