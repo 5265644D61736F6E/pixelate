a25c8585e934cf60a81714dfa657ef5d1a7d18b7
5265644D61736F6E <r2acayan@gmail.com>
2020-04-07 11:51:51 -0400

    completed pass 1, started pass 2 of image

--- auto.c
+++ auto.c
@@ -26,10 +26,9 @@ void pixelate_auto(unsigned char* buf,int channels,int c_width,int c_height,int 
   colored = malloc((c_width * c_height + 7) / 8);
   memset(colored,255,(c_width * c_height + 7) / 8);
 
+  // pass 1 of buf to choose colors to be included
   for (int y1 = 0;y1 < c_height;y1++)
     for (int x1 = 0;x1 < c_width;x1++) {
-      printf("%u:%u\n",x1,y1);
-
       for (int y2 = -2;y2 <= 2;y2++)
 	for (int x2 = -2;x2 <= 2;x2++)
 	  if ((x2 || y2) && x1 + x2 >= 0 && y1 + y2 >= 0 && x1 + x2 < c_width && y1 + y2 < c_height) {
@@ -39,26 +38,33 @@ void pixelate_auto(unsigned char* buf,int channels,int c_width,int c_height,int 
 	    color_min = malloc(channels);
 	    color_max = malloc(channels);
 
-	    printf("%d:%d:\t",x2,y2);
-
+	    // create range for the condition to disable the color export
 	    for (int i = 0;i < channels;i++) {
 	      color_min[i] = (buf[(y1 * c_width + x1) * channels + i]
-			 + buf[((y1 + y2) * c_width + (x1 + x2)) * channels + i]) / 2;
+			    + buf[((y1 + y2) * c_width + (x1 + x2)) * channels + i]) / 2;
 	      color_max[i] = (buf[(y1 * c_width + x1) * channels + i]
-			 + buf[((y1 + y2) * c_width + (x1 + x2)) * channels + i] + 1) / 2;
-
-	      printf("%u\t",color_min[i]);
+			    + buf[((y1 + y2) * c_width + (x1 + x2)) * channels + i] + 1) / 2;
 	    }
-
-	    printf("\n");
 
 	    for (int y3 = -1;y3 < 1;y3++)
 	      for (int x3 = -1;x3 < 1;x3++)
 		if (abs(y2 + y3) == 1 && abs(x2 + x3) == 1 && (x3 != 0 || y3 != 0)) {
-		  
+		  char reset = 1;
+
+		  for (int i = 0;i < channels;i++)
+		    // check if the pixels aren't a gradient
+		    if (buf[((y1 + y2 + y3) * c_width + x1 + x2 + x3) * channels + i] < color_min[i]
+		     || buf[((y1 + y2 + y3) * c_width + x1 + x2 + x3) * channels + i] > color_max[i])
+		      reset = 0; // there is no gradient on this channel so the color shouldn't be disabled
+
+		  // disable exporting the color
+		  if (reset)
+		    bit_reset(colored,(y1 + y2 + y3) * c_width + x1 + x2 + x3);
 		}
 	  }
-
-      printf("\n");
     }
+
+  // pass 2 to count colors to export
+
+  int colored_count = 0;
 }
